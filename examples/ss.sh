fb7e5c91f09870dfefd060573c7f20390565eb23
pablo <pablo@fixpoint.(none)>
2012-12-10 14:48:16 -0300

    Modified compiler script

--- examples/ss.sh
+++ examples/ss.sh
@@ -10,7 +10,7 @@ RES={_}
 ../tools/s2cr.py ${1/.ss/.s} && \
 ../tools/simp_cr.py ${1/.ss/.cr} -o ${1/.ss/.simple.cr} ${TR} -t "{Z}" ${RES} && \
 ../tools/crc.py ${1/.ss/.simple.cr} && \
-gcc -o ${1/.ss/.bin} ${1/.ss/.simple.c} && \
+gcc -o ${1/.ss/.bin} ${1/.ss/.simple.c} -lgmp && \
 ./${1/.ss/.bin}
 
 #../cratylus.py -s ${1/.ss/.simple.cr}
